91f0f1ce2aae426b1faaf49315903d638f545ce7
Joaomgsa <joaomgsa@gmail.com>
2022-10-30 22:58:03 -0300

    implementando metodo na subclasse

--- src/entities/OutsourcedEmployee.java
+++ src/entities/OutsourcedEmployee.java
@@ -12,4 +12,17 @@ public class OutsourcedEmployee extends Employee{
         super(name, hours, valuePerHour);
         this.additionalCharge = additionalCharge;
     }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment() {
+        super.payment();
+    }
 }
